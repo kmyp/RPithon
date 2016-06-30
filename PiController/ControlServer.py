b340195d544728044959fd1e8925e06bacd40b53
coldm <coldm@ThinkDan2>
2016-06-30 16:55:28 +0900

    controle button

--- PiController/ControlServer.py
+++ PiController/ControlServer.py
@@ -12,25 +12,16 @@ def main():
     port = 10007
     bufsize = 128
     TIME = 60000
-#     PWM.setup()
     GPIO.setmode(GPIO.BCM)
-    
 
-    pin_num = [5,6,13,19]
-#     for i in range(0,4):
-#         PWM.init_channel(i,15000)
-#         
-    for i in range(0,4):
-#         PWM.add_channel_pulse(i,pin_num[i],0,100)
+    pin_num = [5,6,13,19,26]
+    for i in range(0,5):
         GPIO.setup(pin_num[i], GPIO.OUT)
-#     GPIO.setup(21,GPIO.OUT)
-#     pwmpin= GPIO.PWM(21,500)
-#     pwmpin.start(50)
-    pin = [GPIO.PWM(5,50),GPIO.PWM(6,50),GPIO.PWM(13,50),GPIO.PWM(19,50)]
-    
-    for i in range(0,4):
+    pin = [GPIO.PWM(5,50),GPIO.PWM(6,50),GPIO.PWM(13,50),GPIO.PWM(19,50),GPIO.PWM(26,50)]
+
+    for i in range(0,5):
         pin[i].start(7.5)
-        
+
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     with closing(sock):
         sock.bind((host, port))
@@ -39,23 +30,19 @@ def main():
             if cmd=="stop":
                 print("stop")
                 for i in range(0,4):
-                    pin[i].stop()       
-#                 pwmpin.stop()             
+                    pin[i].stop()
                 return
 
-            cmd = cmd.split(",")       
-            print(cmd)     
-            
-            for i in range(0,4):
-#                 PWM.clear_channel(i)
-#                 pin[i].ChangeFrequency(int(1.0/float(cmd[i*2+1])*1000000)+100)
+            cmd = cmd.split(",")
+            print(cmd)
+
+            for i in range(0,5):
                 pin[i].ChangeDutyCycle(float(float(cmd[i*2+1])/200))
                 print(float(float(cmd[i*2+1])/200))
-#                 PWM.add_channel_pulse(i,pin_num[i],0,int(cmd[i*2+1])/10)
-            
+
     return
 
-    
+
 if __name__ == '__main__':
   main()
   PWM.cleanup()
